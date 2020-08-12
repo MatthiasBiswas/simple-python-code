3f1cc5b3452d9c5aab3b48cb912bf95271ca69e1
MatthiasBiswas <49491024+MatthiasBiswas@users.noreply.github.com>
2020-08-12 19:13:15 +0600

    Add files via upload

---
+++ simple python.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # simple python code
+# 
+
+# In[26]:
+
+
+def area_triangle(base, heihgt):
+    return (base * height)/2
+
+area_a = area_triangle(7,2)
+area_b = area_triangle(9,6)
+area_sum = area_a + area_b
+print("the area is: " + str(area_sum))
+
+
+# In[ ]:
+
+
+
+
