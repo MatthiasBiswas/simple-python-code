5bc92edaa026bbbd5e622cc594eee348a4701f03
MatthiasBiswas <49491024+MatthiasBiswas@users.noreply.github.com>
2020-08-12 19:22:39 +0600

    Update simple python.py

--- simple python.py
+++ simple python.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # simple python code
-# 
-
-# In[26]:
-
-
 def area_triangle(base, heihgt):
     return (base * height)/2
 
@@ -14,10 +5,3 @@ area_a = area_triangle(7,2)
 area_b = area_triangle(9,6)
 area_sum = area_a + area_b
 print("the area is: " + str(area_sum))
-
-
-# In[ ]:
-
-
-
-
